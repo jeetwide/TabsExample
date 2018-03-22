ac269903ce511e1ce0345641a0057cc608863126
jeetwide <wide.angle456@gmail.com>
2018-03-22 18:58:54 +0530

    added Intro/ Bottom Bar

--- app/src/main/java/com/example/jeetendraachtani/tabsexample/ActWelcome.java
+++ app/src/main/java/com/example/jeetendraachtani/tabsexample/ActWelcome.java
@@ -7,12 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import com.crashlytics.android.Crashlytics;
 import com.example.jeetendraachtani.tabsexample.Analytics.Main2Activity;
+import com.example.jeetendraachtani.tabsexample.BottomBar.ActWelcomeBottomBar;
+import com.example.jeetendraachtani.tabsexample.IntroScreens.SplashActivity;
 
 import io.fabric.sdk.android.Fabric;
 
 public class ActWelcome extends AppCompatActivity {
 
-    Button btn_tab,btn_settings,btn_analytics;
+    Button btn_tab,btn_settings,btn_analytics,btn_intro,btn_bottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,8 @@ public class ActWelcome extends AppCompatActivity {
         btn_tab=findViewById(R.id.btn_tab);
         btn_settings=findViewById(R.id.btn_settings);
         btn_analytics=findViewById(R.id.btn_analytics);
+        btn_intro=findViewById(R.id.btn_intro);
+        btn_bottomBar=findViewById(R.id.btn_bottomBar);
         btn_tab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,6 +42,18 @@ public class ActWelcome extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(ActWelcome.this, Main2Activity.class));
+            }
+        });
+        btn_intro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ActWelcome.this, SplashActivity.class));
+            }
+        });
+        btn_bottomBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ActWelcome.this, ActWelcomeBottomBar.class));
             }
         });
     }
